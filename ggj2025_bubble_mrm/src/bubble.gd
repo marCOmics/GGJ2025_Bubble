485bd4cdb835fc5a37ee902d7cc62dcfdc8debfb
marCOmics <48869977+marCOmics@users.noreply.github.com>
2025-01-24 20:36:44 +0100

    Added Background

--- ggj2025_bubble_mrm/src/bubble.gd
+++ ggj2025_bubble_mrm/src/bubble.gd
@@ -8,4 +8,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	const AMPLITUDE : float = 100
+	#var freq : float = 10.0
+	#offset.x = cos(delta*freq) * AMPLITUDE
 	pass
+
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		#print("Offset: " + str(offset.x)) #event.position)
+		offset.y += 10
